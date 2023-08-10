2c5f04c0385109ba1939b052826f45dc01b24a0d
Zeikuart <acerozed@gmail.com>
2023-08-10 02:32:19 -0600

    Components and Pages

---
+++ Task-Manager/src/app/pages/task-manager/task-manager.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task-manager',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-manager.component.html',
+  styleUrls: ['./task-manager.component.css']
+})
+export class TaskManagerComponent {
+
+}
