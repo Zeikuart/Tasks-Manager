f025d5b247fdd44dcc935fa7247f78b467f548d1
Zeikuart <acerozed@gmail.com>
2023-08-22 01:36:26 -0600

    Updatre

--- Task-Manager/src/app/app.component.ts
+++ Task-Manager/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TaskManagerViewComponent } from './pages/task-manager/task-manager-view/task-manager-view.component';
+import { NavComponent } from './shared/sidenav/nav/nav.component';
+import { HeaderViewComponent } from './shared/header/header-view/header-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TaskManagerViewComponent],
+  imports: [CommonModule, RouterOutlet, TaskManagerViewComponent, NavComponent, HeaderViewComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
